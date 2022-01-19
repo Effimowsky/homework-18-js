07d80818068868bdbf5254f177d54b9d5beeccbf
Effimowsky <o.effimowsky@gmail.com>
2022-01-19 18:51:52 +0400

    update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 // Homework-1
 
-function startClock() {
-  var currentDate = new Date();
-
-  time =
-    currentDate.getHours() +
-    ":" +
-    currentDate.getMinutes() +
-    ":" +
-    currentDate.getSeconds();
-
-  document.getElementById("p").innerHTML = time;
-
-  setTimeout(startClock, 1000);
+function currentTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  var midday = "AM";
+  midday = hour >= 12 ? "PM" : "AM";
+  hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("p").innerText =
+    hour + ":" + min + ":" + sec + "" + midday;
+  var t = setTimeout(currentTime, 1000);
 }
 
-startClock();
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+currentTime();
 
 // Homework-2
